a468202dd332b7f994c14b57ede3164f042c06ab
Diego Ferigo <dieghen89@gmail.com>
2013-03-04 18:27:04 +0100

    Use internal function for the preset load. This commit solves the first preset detection for the preset toggle after the boot

--- eeepc-bootup.sh
+++ eeepc-bootup.sh
@@ -6,10 +6,10 @@ is_root
 #If the cable is plugged
 is_ac_plugged
 if [ $? -eq 0 ] ; then
-	eeepc-power-manager.sh -p $PRESET_AC_PLUG
+	apply_preset $PRESET_AC_PLUG
 	sendlog "Boot with $PRESET_AC_PLUG profile"
 #If the cable is unplugged
 else
-	eeepc-power-manager.sh -p $PRESET_AC_UNPLUG
+	apply_preset $PRESET_AC_UNPLUG
 	sendlog "Boot with $PRESET_AC_UNPLUG profile"
 fi
